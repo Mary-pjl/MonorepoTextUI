170708e6fc445d0ab40d90526afcb18da6484454
Mary-pjl <2904831485@qq.com>
2025-11-10 00:47:20 +0800

    chore: clean ignored files from git tracking

--- node_modules/.pnpm/node_modules/site/node_modules/mary-test-ui/es/src/index.mjs
+++
@@ -1,4 +0,0 @@
-import { MButton as r } from "./button/index.mjs";
-export {
-  r as MButton
-};
--- packages/testUI/es/utils/index.mjs
+++
@@ -1,7 +0,0 @@
-const a = (n) => (n.install = (t) => {
-  const e = n.name;
-  t.component(e, n);
-}, n);
-export {
-  a as withInstall
-};
