1a0f2d4b57f3f5cda2a64afe4a249cc0d4a1fdcf
Mary-pjl <2904831485@qq.com>
2025-11-10 00:48:32 +0800

    chore: clean ignored files from tracking

--- package/es/src/button/index.mjs
+++
@@ -1,7 +0,0 @@
-import { withInstall as t } from "../../utils/index.mjs";
-import o from "./index.vue.mjs";
-const a = t(o);
-export {
-  a as MButton,
-  a as default
-};
--- package/lib/src/button/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperties(exports,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}});const t=require("../../utils/index.js"),u=require("./index.vue.js"),e=t.withInstall(u.default);exports.MButton=e;exports.default=e;
